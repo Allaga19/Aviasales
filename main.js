ffe6b78202ec1fd72d9478cdc7519a4228fdc32e
Allaga19 <gavriushina.alla@yandex.ru>
2020-03-20 14:31:43 +0300

    Создали массив городов

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const formSearch = document.querySelector('.form-search'),
     dropdownDropdownCitiesFrom = document.querySelector('dropdown dropdown__cities-from'),
     inputCitiesTo = document.querySelector('input__cities-to'),
     dropdownCitiesTo = document.querySelector('dropdown__cities-to'),
-    inputDateDepart = document.querySelector('input__date-depart'),
-    ;
+    inputDateDepart = document.querySelector('input__date-depart');
 
-
-//console.log(formSearch);
+// Создаём мфссивы городов
+const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Керч',
+ 'Волгоград', 'Самара', 'Днепропетровск', 'Екатеренбург', 'Одесса', 'Ухань', 'Шымкен', 'Нижний Новгород', 'Калининград',
+  'Вроцлав', 'Ростов-на дону']
